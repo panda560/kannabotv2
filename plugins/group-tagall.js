d9e6bb22be42e4c25e885f6c6602bcb9714c550b
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-09-11 04:20:33 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,14 +1,25 @@
-let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, {
-        mentions: users
-    })
+let handler = async(m, { conn, text, participants }) => {
+
+  let teks = `✾PRATAMA - MD✾ ${text ? text : ' '}\n\n╔─── ᚛۞ 𝐓𝐀𝐆 𝐀𝐋𝐋 ۞᚜ ───╗\n`
+
+		      	for (let mem of participants) {
+
+		            teks += `╠➣@${mem.id.split('@')[0]}\n`
+
+				}
+
+                conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+
 }
 
-handler.help = ['tagall']
+handler.help = ['tagall <pesan>']
+
 handler.tags = ['group']
-handler.command = ['tagall']
-handler.admin = true
+
+handler.command = /^(tagall)$/i
+
 handler.group = true
+
+handler.admin = true
 
 export default handler
